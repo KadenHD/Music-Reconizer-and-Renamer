62a61cd8b907d183109a54c6248453423da6c945
KadenHD <71968563+KadenHD@users.noreply.github.com>
2024-04-22 15:19:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Music-Recognizer-Namer-Saver',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'shazamio==0.5.1',
+        'tqdm==4.66.2',
+        'PyQt5==5.15.10',
+    ],
+    python_requires='>=3.12.3',
+    author='CLEMENT Pierre',
+    author_email='pierraored@gmail.com',
+    description='Use Shazam to find the musics title and author to copy files with the name in a folder.',
+)
